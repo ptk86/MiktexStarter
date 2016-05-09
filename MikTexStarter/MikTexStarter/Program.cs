2ccdf5f7fc8d60555b3dc60c39f133f3959f1876
Piotr Kiliński <abovo86@hotmail.com>
2016-05-09 19:51:02 +0200

    Added MikTex pdf creation logic using Process. Before running the program: 1. Install MikTex 2. Create directory : "C:\HelloWorldMikTex" 3. Call your brother on Skype

--- MikTexStarter/MikTexStarter/Program.cs
+++ MikTexStarter/MikTexStarter/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,25 @@ namespace MikTexStarter
     {
         static void Main(string[] args)
         {
+         
+            Process process = new Process();
+            process.StartInfo.FileName = "texify";
+            process.StartInfo.Arguments = String.Format("-p -b -q -c HelloWorld.tex");
+            process.StartInfo.WorkingDirectory = @"C:\HelloWorldMikTex";
+            process.StartInfo.RedirectStandardOutput = true;
+            process.StartInfo.RedirectStandardError = true;
+            process.StartInfo.UseShellExecute = false;
+            process.Start();
+            process.WaitForExit();
+            if (process.ExitCode == 0)
+            {
+                Console.WriteLine("Success!");
+            }
+            else
+            {
+                Console.WriteLine("Error!");
+            }
+            Console.ReadKey();
         }
     }
 }
